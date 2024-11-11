66fd5c422f34f347671775e9419220290fb1325b
vidyashree-v123 <vidyashreev2004@gmail.com>
2024-11-11 12:24:02 +0530

    all program

---
+++ additional.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+void main(){
+    int a=10;
+    a=a+b;
+    b=a-b;
+    a=a-b;
+    printf("a=%d\nb=%",a,b);
+}
---
+++ palindrome.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+void main(){
+    int f=12551,x=0,y=0,a=f;
+    while(f>0){
+        x=f%10;
+        y=y*10+x;
+        f=f/10;
+    }
+    if(a==y)
+    {
+        printf("yes");
+    }else{printf("no");}
